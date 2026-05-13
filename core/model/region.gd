2468f28b7b6076e1b04889f51b14beee1ff65057
samuel.murphy <LionhunterUK@gmail.com>
2026-05-13 21:30:26 +0100

    Update region.gd

--- core/model/region.gd
+++ core/model/region.gd
@@ -12,27 +12,33 @@ var has_factory: bool
 var is_land: bool = false
 var adjacent: Array[String] = []
 
+
 static func from_dict(data: Dictionary) -> Region:
 	var region := Region.new()
-	region.id = data.get("id", "")
+	region.id = str(data.get("id", ""))
 	region.name = data.get("name", "")
 	region.type = data.get("type", "")
-	region.ipc_value = data.get("ipc_value", 0)
-	region.owner_faction_id = data.get("owner_faction_id", "")
-	region.is_capital = data.get("is_capital", false)
-	region.has_factory = data.get("has_factory", false)
+	region.ipc_value = int(data.get("ipc_value", 0))
+	region.owner_faction_id = str(data.get("owner_faction_id", ""))
+	region.is_capital = bool(data.get("is_capital", false))
+	region.has_factory = bool(data.get("has_factory", false))
 
 	if data.has("is_land"):
 		region.is_land = bool(data["is_land"])
 	else:
 		region.is_land = (region.type == "land")
 
-	region.adjacent = data.get("adjacent", [])
+	var adj = data.get("adjacent", [])
+	region.adjacent = []
+	for a in adj:
+		region.adjacent.append(str(a))
 
 	return region
 
+
 func is_land_region() -> bool:
 	return is_land
+
 
 func is_sea_region() -> bool:
 	return not is_land
